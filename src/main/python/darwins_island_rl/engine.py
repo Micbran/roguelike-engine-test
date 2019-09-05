93d20422ad78d1374b773041102bbce84e2835d9
Brandon <micbran22@gmail.com>
2019-09-05 15:22:25 -0500

    Phasing in Curses Implementation

--- src/main/python/darwins_island_rl/engine.py
+++ src/main/python/darwins_island_rl/engine.py
@@ -1,12 +1,34 @@
-import tcod
+import curses
+from curses import wrapper
 import logging
+import os
 
-logger = logging.getLogger(__name__)
+SCREEN_WIDTH = 80
+SCREEN_HEIGHT = 50
+RESOURCE_FOLDER_LOCATION = os.path.join('resources')
+logging.basicConfig(level=logging.DEBUG,
+                    filename='game_log.log',
+                    filemode='w',
+                    format='%(name)s:%(levelname)s: %(message)s')
 
 
-def main():
-    logger.info("Game start.")
+def main(main_screen):
+    logging.info("-----------Game start-----------")
+    main_screen.clear()
+    curses.noecho()
+    curses.cbreak()
+    main_screen.keypad(True)
+    quit_console(main_screen)
+    logging.info(main_screen)
+    logging.info("Game End")
+
+
+def quit_console(console_window):
+    curses.nocbreak()
+    console_window.keypad(False)
+    curses.echo()
+    curses.endwin()
 
 
 if __name__ == '__main__':
-    main()
+    wrapper(main)
--- src/unittests/engine_tests.py
+++ src/unittests/engine_tests.py
@@ -4,6 +4,3 @@ import unittest
 class EngineTests(unittest.TestCase):
     def setUp(self):
         pass
-
-    def test_always_pass(self):
-        self.assertTrue(True)
