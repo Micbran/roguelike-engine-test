bf43aede6b87778e2c7d2d369da97a6162c8c063
Brandon <micbran22@gmail.com>
2019-12-17 10:52:55 -0600

    Revert "Phasing in Curses Implementation"
    This reverts commit 93d20422ad78d1374b773041102bbce84e2835d9.

--- src/main/python/darwins_island_rl/engine.py
+++ src/main/python/darwins_island_rl/engine.py
@@ -1,34 +1,12 @@
-import curses
-from curses import wrapper
+import tcod
 import logging
-import os
 
-SCREEN_WIDTH = 80
-SCREEN_HEIGHT = 50
-RESOURCE_FOLDER_LOCATION = os.path.join('resources')
-logging.basicConfig(level=logging.DEBUG,
-                    filename='game_log.log',
-                    filemode='w',
-                    format='%(name)s:%(levelname)s: %(message)s')
+logger = logging.getLogger(__name__)
 
 
-def main(main_screen):
-    logging.info("-----------Game start-----------")
-    main_screen.clear()
-    curses.noecho()
-    curses.cbreak()
-    main_screen.keypad(True)
-    quit_console(main_screen)
-    logging.info(main_screen)
-    logging.info("Game End")
-
-
-def quit_console(console_window):
-    curses.nocbreak()
-    console_window.keypad(False)
-    curses.echo()
-    curses.endwin()
+def main():
+    logger.info("Game start.")
 
 
 if __name__ == '__main__':
-    wrapper(main)
+    main()
--- src/unittests/engine_tests.py
+++ src/unittests/engine_tests.py
@@ -4,3 +4,6 @@ import unittest
 class EngineTests(unittest.TestCase):
     def setUp(self):
         pass
+
+    def test_always_pass(self):
+        self.assertTrue(True)
